8b0d028a2fe11e098306053f3b82af71a611b2cf
danielmoralesp <daniel_moralesp@hotmail.com>
2017-09-05 15:12:45 -0500

    Ejercicio de strings

--- ex1.js
+++ ex1.js
@@ -4,3 +4,13 @@ let height = 7
 let triangleArea = (base, height) => base * height / 2
 
 console.log(`El area de un triangulo de base ${base} y altura ${height} es: ${triangleArea(base, height)}`)
+
+ // Ojo con la nueva sintaxis de es6, con la comilla y la interpolacion
+
+const starWars7 = 'Starwars el despertar de la fuerza'
+const pgStarWars7 = 13
+
+const nameDaniel = 'Daniel'
+const ageDaniel = 34
+
+ 
