446a8e5d65d645c508c5a153a6b0a9dfe71c7cd3
danielmoralesp <daniel_moralesp@hotmail.com>
2017-09-05 14:21:53 -0500

    Initial commit

---
+++ ex1.js
@@ -0,0 +1,6 @@
+let base = 5
+let height = 7
+
+let triangleArea = (base, height) => base * height / 2
+
+console.log(`El area de un triangulo de base ${base} y altura ${height} es: ${triangleArea(base, height)}`)
