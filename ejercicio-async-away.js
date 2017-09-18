b4890ca0bbbb105469a0208a43ba224077038ab4
danielmoralesp <daniel_moralesp@hotmail.com>
2017-09-18 13:20:48 -0500

    async away excersice

---
+++ ejercicio-async-away.js
@@ -0,0 +1,15 @@
+function handleError(err) {
+  console.log(`Request failed: ${err}`)
+}
+
+async function getLuke() {
+  try {
+    const response = await fetch('http://swapi.co/api/people/1/')
+    const luke = await response.json()
+    const responseHomeworld = await fetch(luke.homeworld)
+    luke.homeworld = await responseHomeworld.json()
+    console.log(`${luke.name} nació en ${luke.homeworld.name}`)
+  } catch (err) {
+    handleError(err)
+  }
+}
